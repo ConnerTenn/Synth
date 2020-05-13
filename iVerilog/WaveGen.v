f3e86de6d72052652f807b275569a157b72dcd0e
Conner <connertenn@gmail.com>
2020-05-13 04:06:04 -0400

    Switched to using function case statement
    Instead of tri state buffer

--- iVerilog/WaveGen.v
+++ iVerilog/WaveGen.v
@@ -13,12 +13,26 @@ module WaveGen(
 
     reg [7:0] counter = 8'h00;
 
-    //Sawtooth
-    assign Waveform = WaveType==2'b00 ? counter : 8'hZZ;
-    //Square Wave
-    assign Waveform = WaveType==2'b01 ? (counter<=(Frequency>>1) ? 8'h00 : 8'hFF) : 8'hZZ;
-    //Triangle
-    assign Waveform = WaveType==2'b10 ? (counter<=(Frequency>>1) ? counter : (Frequency-counter)+1) : 8'hZZ;
+    // //Sawtooth
+    // assign Waveform = WaveType==2'b00 ? counter : 8'hZZ;
+    // //Square
+    // assign Waveform = WaveType==2'b01 ? (counter<=(Frequency>>1) ? 8'h00 : 8'hFF) : 8'hZZ;
+    // //Triangle
+    // assign Waveform = WaveType==2'b10 ? (counter<=(Frequency>>1) ? counter : (Frequency-counter)+1) : 8'hZZ;
+
+    assign Waveform = WaveTypeSelect(counter, Frequency, WaveType);
+
+    function [7:0] WaveTypeSelect(input [7:0] counter, input [7:0] frequency, input [1:0] wavetype);
+        case(wavetype)
+            //Sawtooth
+            2'b00 : WaveTypeSelect = counter;
+            //Square
+            2'b01 : WaveTypeSelect = (counter<=(Frequency>>1) ? 8'h00 : 8'hFF);
+            //Triangle
+            2'b10 : WaveTypeSelect = (counter<=(Frequency>>1) ? counter : (Frequency-counter)+1);
+            default : WaveTypeSelect = 2'bZ;
+        endcase
+    endfunction
 
     always @ (posedge Clock)
     begin
