dde5c99d03f38233a931a849ab63fda77dd538d6
Conner <connertenn@gmail.com>
2020-05-14 00:44:00 -0400

    Pruned code a bit

--- iVerilog/WaveGen.v
+++ iVerilog/WaveGen.v
@@ -19,9 +19,6 @@ module WaveGen
     input [1:0] WaveType;
     output [WAVE_HIGH_BIT:0] Waveform;
 
-    wire [1:0] wavesel;
-    assign wavesel = Reset ? 2'b00 : WaveType; //Wavetype is set to 0 when reset
-
     reg [WAVE_HIGH_BIT:0] counter = 0;
 
     wire [WAVE_HIGH_BIT:0] counterHalfShifted;
@@ -30,20 +27,20 @@ module WaveGen
     assign counterHalfShifted = counter+(WAVE_MAX>>1);
     assign counterQuarterShifted = counter+(WAVE_MAX>>2);
 
-    assign Waveform = WaveTypeSelect(counterHalfShifted, counterQuarterShifted, wavesel);
-    function [7:0] WaveTypeSelect(input [WAVE_HIGH_BIT:0] counterhalf, input [WAVE_HIGH_BIT:0] counterquart, input [1:0] wavetype);
+    assign Waveform = WaveTypeSelect(counterHalfShifted, counterQuarterShifted, Reset?2'b00:WaveType);
+    function [7:0] WaveTypeSelect(input [WAVE_HIGH_BIT:0] countHalfShft, input [WAVE_HIGH_BIT:0] countQuartShft, input [1:0] wavetype);
         case(wavetype)
             //Sawtooth
-            2'b00 : WaveTypeSelect = counterhalf;
+            2'b00 : WaveTypeSelect = countHalfShft;
             //Square
-            2'b01 : WaveTypeSelect = (counterhalf>=(WAVE_MAX>>1) ? WAVE_MAX : 8'h00);
+            2'b01 : WaveTypeSelect = (countHalfShft>=(WAVE_MAX>>1) ? WAVE_MAX : 8'h00);
             //Triangle
-            2'b10 : WaveTypeSelect = (counterquart<=(WAVE_MAX>>1) ? counterquart : (WAVE_MAX-counterquart))<<1;// + (WAVE_MAX>>2);
+            2'b10 : WaveTypeSelect = (countQuartShft<=(WAVE_MAX>>1) ? countQuartShft : (WAVE_MAX-countQuartShft))<<1;// + (WAVE_MAX>>2);
             default : WaveTypeSelect = 2'bZ;
         endcase
     endfunction
 
-    always @ (posedge Clock or Reset)
+    always @(posedge Clock or Reset)
     begin
         if (Reset==1) 
         begin
