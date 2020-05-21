819d6750bcbc540040292ca67d6df1c07ebc49c2
Conner <connertenn@gmail.com>
2020-05-20 22:14:16 -0400

    Added block ram

--- iVerilog/FilterBench.v
+++ iVerilog/FilterBench.v
@@ -2,9 +2,20 @@
 
 `timescale 1 ms / 1 us
 
+
 module FilterBench;
-    
+
+    parameter MIN_STEP = 0.0005;
+    parameter TWO_STEP = MIN_STEP*2;
+
     reg clock = 0;
+
+    reg dataclock = 0;
+    reg [7:0] addr = 0;
+    reg [7:0] data = 0;
+    reg rw = 0;
+    
+
 
     initial
     begin
@@ -12,7 +23,31 @@ module FilterBench;
         $dumpvars;
         $display("Running Simulation...");
         
-        #100
+        #1
+
+        addr <= 1;
+        data <= 5;
+        rw <= 1;
+
+        dataclock <= 1; #TWO_STEP dataclock <= 0; #TWO_STEP
+
+        addr <= 2;
+        data <= 1;
+        rw <= 1;
+
+        dataclock <= 1; #TWO_STEP dataclock <= 0; #TWO_STEP
+
+        addr <= 1;
+        rw <= 0;
+
+        dataclock <= 1; #TWO_STEP dataclock <= 0; #TWO_STEP
+
+        addr <= 2;
+        rw <= 0;
+
+        dataclock <= 1; #TWO_STEP dataclock <= 0; #TWO_STEP
+
+        #1
 
         $display("Simulation Complete");
 
@@ -21,7 +56,16 @@ module FilterBench;
 
     always
     begin
-        #0.0005 clock = !clock;
+        #MIN_STEP clock = !clock;
     end
+
+    wire [7:0] dataio = rw==1 ? data : 8'hZZ;
+
+    Ram ram (
+        .Clock(dataclock),
+        .Address(addr),
+        .Data(dataio),
+        .ReadWrite(rw)
+    );
 
 endmodule
--- iVerilog/Ram.v
+++ iVerilog/Ram.v
@@ -1,8 +1,7 @@
 
 
 module Ram(
-    Clock, 
-    Reset,
+    Clock,
     Address, Data,
     ReadWrite
 );
@@ -10,5 +9,22 @@ module Ram(
     input [7:0] Address;
     inout [7:0] Data; 
     input ReadWrite;
+
+    reg [7:0] Memory [2**16-1:0];
+
+    reg [7:0] data = 0;
+    assign Data = ReadWrite==0 ? data : 8'hZZ;
+
+    always @(posedge Clock)
+    begin
+        if (ReadWrite==1)
+        begin
+            Memory[Address] <= Data;
+        end
+        else
+        begin
+            data <= Memory[Address];
+        end
+    end
 
 endmodule
